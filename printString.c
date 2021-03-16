19a3e8edb8451b9137ce107e83009e89f7604093
lamine <mokhtarjemli2@gmail.com>
2021-03-16 07:51:42 -0700

    fix

--- printString.c
+++ printString.c
@@ -1,56 +1,16 @@
-[![](https://i.ibb.co/jH2zPmM/printffff.jpg)](https://i.ibb.co/jH2zPmM/printffff.jpg)
-# _printf 💬 
-The printf function is used to display on the screen window a text which follows a format defined by the ... If this is not the case, C chooses to obey the forma code and is used to print ("character,string,float,integer,octal,and hexadecimal values")
+#include "holberton.h"
+/**
+ *  * _print_string - Prints a string.
+ *   * @sl: specifiers list.
+ *    *
+ *     */
+int printString(va_list sl)
+{
+	  int j;
+	    char *s;
 
-
-## projet requiment 
-1. Linux
-2. git
-3. gcc 4.8.4
-
-### Concept of the project :
-1 how to work in a group and have a good experience
-2 How to apply the variadic function
-3 How printf works
-4 how to use git in team setting
-
-#### Function prototype
-int _printf(const char *format, ...);
-
-#### Examples
-**Decimal :
-**
-input : _printf("%d",1000);
-
-output : 1000
-**Integer** :
-
-input : _printf("%i",20);
-
-output : 20
-**Character** :
-
-input : _printf("%s", this is a string);
-
-output : this is a string
-**String**:
-
-input : _printf("%s", this is a string);
-
-output : this is a string
-
-#### Files Required :| 💻  |  💻 |
-|  file | description  |
-| ------------ | ------------ |
-| _putchar  |  _putchar - writes the character c to stdout |
-| main.c  |main - Entry point |
-| _holberton.h  | print_string - Prints a string. |
-|  printChar.c|  print_char - print charachter|
-| printInteger.c | printInteger - prints an integer.|
-| print_string.c  |  print_string - Prints a string  |
-|  printPercent.c | _percentage - Print percentage character |
-|  _printf | printf - produce output  |
-#### Authors :
-rami louhibi  https://github.com/ramii02/holbertonschool-low_level_programming 
-lamine jemli https://github.com/laminejemli/holbertonschool-low_level_programming
-ahmed belhssan https://github.com/ahmed77-jpg
+	      s = va_arg(sl, char *);
+	        for (j = 0; s[j] != '\0'; j++)
+			    _putchar(s[j]);
+		  return (j);
+}
